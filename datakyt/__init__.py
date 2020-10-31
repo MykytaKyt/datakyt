3359789dbdc2647ca07187064d65e61a7e0bba0e
Anton Artiukh <artyukhanton@gmail.com>
2020-10-31 16:41:38 +0200

    [#14] Create project skeleton (#16)
    * Add project skeleton
    
    * Add code of conduct
    
    * Add license
    
    * Add gitigore
    
    * Update github template
    
    * Update git ignore
    
    * Add directory for queries

---
+++ datakyt/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import logging
+import os
+import random
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s.%(msecs)03d[%(process)d:%(thread)d:%(name)s:%(lineno)d] %(levelname)s %('
+                           'message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S')
