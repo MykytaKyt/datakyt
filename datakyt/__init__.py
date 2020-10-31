589773110aab530c3c392f6d0cca5274de79f651
Anton Artiukh <artyukhanton@gmail.com>
2020-10-31 13:24:01 +0200

    Add gitigore

--- datakyt/__init__.py
+++ datakyt/__init__.py
@@ -5,3 +5,7 @@ import random
 
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s.%(msecs)03d[%(process)d:%(thread)d:%(name)s:%(lineno)d] %(levelname)s %('
+                           'message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S')
