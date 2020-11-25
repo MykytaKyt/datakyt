98a9641a7a7b47b85fcb4793abc1b41b44150112
Alex-ty1 <oleksander.zhytnyk@gmail.com>
2020-11-25 22:51:22 +0200

    fix logger

--- datakyt/db/dump_db.py
+++ datakyt/db/dump_db.py
@@ -1,4 +1,12 @@
 import subprocess
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s.%(msecs)03d[%(process)d:%(thread)d:%(name)s:%(lineno)d] %(levelname)s %('
+                           'message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S')
 
 
 def backup_postgres_db(host, database_name, port, user, password, dest_file):
@@ -16,12 +24,13 @@ def backup_postgres_db(host, database_name, port, user, password, dest_file):
         )
         output = process.communicate()[0]
         if int(process.returncode) != 0:
-            print('Command failed. Return code : {}'.format(process.returncode))
+            logger.error('Command failed. Return code : {}'.format(process.returncode))
             exit(1)
         return output
     except Exception as e:
-        print(e)
+        logger.error(e)
 
 
 if __name__ == '__main__':
+
     backup_postgres_db('localhost', 'datakyt', '5432', 'datakyt_admin', 'password', 'out2.txt')
