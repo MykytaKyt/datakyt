83688f14b59062cc53d6d16418642d3b5de56579
Alex-ty1 <oleksander.zhytnyk@gmail.com>
2020-11-26 22:07:54 +0200

    add documentation

--- datakyt/db/dump_db.py
+++ datakyt/db/dump_db.py
@@ -9,28 +9,36 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%Y-%m-%d %I:%M:%S')
 
 
-def backup_postgres_db(host, database_name, port, user, password, dest_file):
+def backup_postgres_db(host, database_name, port, user, password, test_file):
     """
   Backup postgres db to a file.
+
+   Parameters
+   ----------
+   host: the host to connect to (default localhost).
+   database_name: the name of database.
+   port: the port to connect on (default 5432).
+   user: the user account to connect as (default admin).
+   password: the password for the user account to connect as.
+   test_file: the name of the file in which it will be stored back up.
+
   """
     try:
         process = subprocess.Popen(
             ['pg_dump',
              '--dbname=postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, database_name),
              '-Fc',
-             '-f', dest_file,
+             '-f', test_file,
              '-v'],
             stdout=subprocess.PIPE
         )
-        output = process.communicate()[0]
         if int(process.returncode) != 0:
             logger.error('Command failed. Return code : {}'.format(process.returncode))
             exit(1)
-        return output
     except Exception as e:
         logger.error(e)
 
 
 if __name__ == '__main__':
 
-    backup_postgres_db('localhost', 'datakyt', '5432', 'datakyt_admin', 'password', 'out2.txt')
+    backup_postgres_db('localhost', 'datakyt', '5432', 'datakyt_admin', 'password', 'backup.txt')
