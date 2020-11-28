fd05e7223ddf73c979ad4ec14ce87150ed4eba4b
Alex-ty1 <oleksander.zhytnyk@gmail.com>
2020-11-28 19:28:59 +0200

    remove all brackets in the description

--- datakyt/db/dump_db.py
+++ datakyt/db/dump_db.py
@@ -9,16 +9,16 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%Y-%m-%d %I:%M:%S')
 
 
-def backup_postgres_db(host, database_name, port, user, password, backup_file_path):
+def backup_postgres_db(database_name, host,  port, user, password, backup_file_path):
     """
   Backup postgres db to a file.
 
    Parameters
    ----------
-   host: the host to connect to (default localhost).
+   host: the host to connect to .
    database_name: the name of database.
-   port: the port to connect on (default 5432).
-   user: the user account to connect as (default admin).
+   port: the port to connect on .
+   user: the user account to connect as .
    password: the password for the user account to connect as.
    backup_file_path: the name of the file in which it will be stored back up.
 
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     time = datetime.now()
     time = time.strftime("%m-%d-%Y-%H_%M")
     dump_path = f'backup_datakyt_{time}.sql'
-    backup_postgres_db('localhost', 'datakyt', '5432', 'datakyt_admin', 'password', dump_path)
+    backup_postgres_db('datakyt', 'localhost', '5432', 'datakyt_admin', 'password', dump_path)
