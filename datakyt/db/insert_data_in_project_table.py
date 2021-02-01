be49e05dd05205a126c4ea816795551274ac2ec4
Alex-ty1 <oleksander.zhytnyk@gmail.com>
2021-02-01 22:38:20 +0200

    add line

--- datakyt/db/insert_data_in_project_table.py
+++ datakyt/db/insert_data_in_project_table.py
@@ -16,7 +16,7 @@ def insert_csv_file_to_database(path, conn):
         reader = csv.reader(f)
         for row in reader:
             array_values_from_file.append([int(row[0]), row[1]])
-            # insert_query = "INSERT INTO project VALUES {}".format(f"({row[0]}, '{row[1]}')")
+            insert_query = "INSERT INTO project VALUES {}".format(f"({row[0]}, '{row[1]}')")
             cur.execute(insert_query)
         conn.commit()
 
