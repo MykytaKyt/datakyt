864f55c103aa7a4a38130f001d594c9dee742ee5
Alex-ty1 <oleksander.zhytnyk@gmail.com>
2021-02-07 18:19:27 +0200

    fix blank space again

--- datakyt/db/import/insert_data_in_project_table_test.py
+++ datakyt/db/import/insert_data_in_project_table_test.py
@@ -41,24 +41,22 @@ class TestDB(unittest.TestCase):
         path_to_db = r'test_data/test_database.db'
 
         sql_create_projects_table = """ CREATE TABLE project(
-                                             id SMALLSERIAL  PRIMARY KEY,
-                                             name text  NOT NULL
+                        id SMALLSERIAL  PRIMARY KEY,
+                        name text  NOT NULL
                                         );"""
 
         sql_create_offices_table = """CREATE TABLE office(
-                                             id SMALLSERIAL  PRIMARY KEY,
-                                             city text       NOT NULL
+                        id SMALLSERIAL  PRIMARY KEY,
+                        city text       NOT NULL
                                     );"""
 
         sql_create_employees_table = """CREATE TABLE employee(
-                                            id SMALLSERIAL  PRIMARY KEY,
-                                            name text       NOT NULL,
-                                            email char(65)  UNIQUE,
-                                            phone varchar(65) UNIQUE,
-                                            project_id	SMALLSERIAL CONSTRAINT project_id_fk REFERENCES project (id)
-                                            ON DELETE CASCADE,
-                                            office_id	SMALLSERIAL CONSTRAINT office_id_fk REFERENCES office (id)
-                                            ON DELETE CASCADE
+                        id SMALLSERIAL  PRIMARY KEY,
+                        name text       NOT NULL,
+                        email char(65)  UNIQUE,
+                        phone varchar(65) UNIQUE,
+                        project_id	SMALLSERIAL CONSTRAINT project_id_fk REFERENCES project (id) ON DELETE CASCADE,
+                        office_id	SMALLSERIAL CONSTRAINT office_id_fk REFERENCES office (id)ON DELETE CASCADE
                                          );"""
 
         sql_create_equipment_type_table = """CREATE TABLE equipment_type(
@@ -67,83 +65,79 @@ class TestDB(unittest.TestCase):
                                           );"""
 
         sql_create_equipment_table = """CREATE TABLE equipment(
-                                        id                  SMALLSERIAL    PRIMARY KEY,
-                                        name                text           NOT NULL,
-                                        warranty            integer        NOT NULL,
-                                        cost                money          NOT NULL CHECK (cost > 0),
-                                        status              varchar(10)    NOT NULL
-                                             CONSTRAINT status_check 
-                                             CHECK(status IN('issued','on reserve','in repair','broken')),
-                                        description         text,
-                                        purchase_date       date           NOT NULL,
-                                        serial_number       varchar(65)    NOT NULL,
-                                        equipment_type_id	SMALLSERIAL
-                                            CONSTRAINT equipment_type_id_fk REFERENCES equipment_type(id) 
-                                            ON DELETE CASCADE
+                        id                  SMALLSERIAL    PRIMARY KEY,
+                        name                text           NOT NULL,
+                        warranty            integer        NOT NULL,
+                        cost                money          NOT NULL CHECK (cost > 0),
+                        status              varchar(10)    NOT NULL
+                                CONSTRAINT status_check CHECK(status IN('issued','on reserve','in repair','broken')),
+                        description         text,
+                        purchase_date       date           NOT NULL,
+                        serial_number       varchar(65)    NOT NULL,
+                        equipment_type_id	SMALLSERIAL
+                                CONSTRAINT equipment_type_id_fk REFERENCES equipment_type(id) ON DELETE CASCADE
                                         );"""
 
         sql_create_equipment_part_table = """CREATE TABLE equipment_part(
-                                        id              SMALLSERIAL     PRIMARY KEY,
-                                        name            text            UNIQUE,
-                                        equipment_id	SMALLSERIAL
-                                             CONSTRAINT equipment_id_fk REFERENCES equipment(id) ON DELETE CASCADE
+                        id              SMALLSERIAL     PRIMARY KEY,
+                        name            text            UNIQUE,
+                        equipment_id	SMALLSERIAL
+                                CONSTRAINT equipment_id_fk REFERENCES equipment(id) ON DELETE CASCADE
                                         );"""
 
         sql_create_software_table = """CREATE TABLE software(
-                                        id      SMALLSERIAL	 PRIMARY KEY,
-                                        name    text         NOT NULL
+                        id      SMALLSERIAL	 PRIMARY KEY,
+                        name    text         NOT NULL
                                         );"""
 
         sql_create_software_license_table = """CREATE TABLE software_license(
-                                        id                  SMALLSERIAL	PRIMARY KEY,
-                                        software_id         SMALLSERIAL
-                                            CONSTRAINT software_id_fk REFERENCES software(id) ON DELETE CASCADE,
-                                        product_key         text        UNIQUE,
-                                        date_of_purchase    date        NOT NULL,
-                                        date_of_expiry	    date        NOT NULL
+                        id                  SMALLSERIAL	PRIMARY KEY,
+                        software_id         SMALLSERIAL
+                                CONSTRAINT software_id_fk REFERENCES software(id) ON DELETE CASCADE,
+                        product_key         text        UNIQUE,
+                        date_of_purchase    date        NOT NULL,
+                        date_of_expiry	    date        NOT NULL
                                         );"""
 
         sql_create_employee_sw_license_table = """CREATE TABLE employee_sw_license(
-                                            id                  SMALLSERIAL 	PRIMARY KEY,
-                                            date_of_issue       date            NOT NULL,
-                                            employee_id         SMALLSERIAL
-                                                CONSTRAINT employee_id_fk REFERENCES employee(id) ON DELETE CASCADE,
-                                            software_license_id	SMALLSERIAL
-                                                CONSTRAINT software_license_id_fk REFERENCES software_license(id) 
-                                                ON DELETE CASCADE
+                        id                  SMALLSERIAL 	PRIMARY KEY,
+                        date_of_issue       date            NOT NULL,
+                        employee_id         SMALLSERIAL
+                                CONSTRAINT employee_id_fk REFERENCES employee(id) ON DELETE CASCADE,
+                        software_license_id	SMALLSERIAL
+                                CONSTRAINT software_license_id_fk REFERENCES software_license(id) ON DELETE CASCADE
                                             );"""
         sql_create_furniture_type_table = """CREATE TABLE furniture_type(
-                                            id      SMALLSERIAL     PRIMARY KEY,
-                                            type    text            UNIQUE
+                        id      SMALLSERIAL     PRIMARY KEY,
+                        type    text            UNIQUE
                                             );"""
 
         sql_create_furniture_table = """CREATE TABLE furniture(
-                                            id                  SMALLSERIAL	PRIMARY KEY,
-                                            furniture_type_id   SMALLSERIAL
-                                                CONSTRAINT furniture_type_id_fk REFERENCES furniture_type (id) 
-                                                ON DELETE CASCADE,
-                                            name                text        NOT NULL,
-                                            warranty            integer	    NOT NULL,
-                                            cost                money 	    NOT NULL CHECK (cost > 0)
+                        id                  SMALLSERIAL	PRIMARY KEY,
+                        furniture_type_id   SMALLSERIAL
+                                CONSTRAINT furniture_type_id_fk REFERENCES furniture_type (id) ON DELETE CASCADE,
+                        name                text        NOT NULL,
+                        warranty            integer	    NOT NULL,
+                        cost                money 	    NOT NULL CHECK (cost > 0)
                                             );"""
 
         sql_create_employee_furniture_table = """CREATE TABLE employee_furniture(
-                                            id	            SMALLSERIAL     PRIMARY KEY,
-                                            date_of_issue   date            NOT NULL,
-                                            furniture_id    SMALLSERIAL
-                                                CONSTRAINT furniture_id_fk REFERENCES furniture (id) ON DELETE CASCADE,
-                                            employee_id	    SMALLSERIAL
-                                                CONSTRAINT employee_id_fk REFERENCES employee (id) ON DELETE CASCADE
+                        id	            SMALLSERIAL     PRIMARY KEY,
+                        date_of_issue   date            NOT NULL,
+                        furniture_id    SMALLSERIAL
+                                CONSTRAINT furniture_id_fk REFERENCES furniture (id) ON DELETE CASCADE,
+                        employee_id	    SMALLSERIAL
+                                CONSTRAINT employee_id_fk REFERENCES employee (id) ON DELETE CASCADE
                                             );"""
 
         sql_create_employee_equipment_table = """CREATE TABLE employee_equipment(
-                                            id                  SMALLSERIAL     PRIMARY KEY,
-                                            employee_id         SMALLSERIAL
-                                                CONSTRAINT employee_id_fk REFERENCES employee(id) ON DELETE CASCADE,
-                                            equipment_id        SMALLSERIAL
-                                                CONSTRAINT equipment_id_fk REFERENCES equipment(id) ON DELETE CASCADE,
-                                            date_of_issue       date            NOT NULL,
-                                            day_of_return       date
+                        id                  SMALLSERIAL     PRIMARY KEY,
+                        employee_id         SMALLSERIAL
+                                CONSTRAINT employee_id_fk REFERENCES employee(id) ON DELETE CASCADE,
+                        equipment_id        SMALLSERIAL
+                                CONSTRAINT equipment_id_fk REFERENCES equipment(id) ON DELETE CASCADE,
+                        date_of_issue       date            NOT NULL,
+                        day_of_return       date
                                             );"""
 
         # create a database connection
